3f7ed69053b3162f376a5d8bcdb496ba851b5295
hitarthk <hitarthk@gmail.com>
2019-11-16 21:11:21 -0800

    removed redundant call to netg in val of train_g and renamed variable

--- train/train_G.py
+++ train/train_G.py
@@ -256,7 +256,7 @@ def val():
     netG.eval()
 
     data_iter_val = iter(dataloader_val)
-    ques_hidden = netE.init_hidden(opt.batchSize)
+    mapped_h_mem = netE.init_hidden(opt.batchSize)
     hist_hidden = netE.init_hidden(opt.batchSize)
 
     i = 0
@@ -304,16 +304,14 @@ def val():
             ques_emb = netW(ques_input, format = 'index')
             his_emb = netW(his_input, format = 'index')
 
-            ques_hidden = repackage_hidden_new(ques_hidden, batch_size)
+            mapped_h_mem = repackage_hidden_new(mapped_h_mem, batch_size)
             hist_hidden = repackage_hidden_new(hist_hidden, his_input.size(1))
 
-            encoder_feat, ques_hidden = netE(ques_emb, his_emb, img_input, \
-                                                ques_hidden, hist_hidden, rnd+1)
-
-            _, ques_hidden = netG(encoder_feat.view(1,-1,opt.ninp), ques_hidden)
+            encoder_feat, mapped_h_mem = netE(ques_emb, his_emb, img_input, \
+                                                mapped_h_mem, hist_hidden, rnd+1)
 
             hidden_replicated = []
-            for hid in ques_hidden:
+            for hid in mapped_h_mem:
                 hidden_replicated.append(hid.view(opt.nlayers, batch_size, 1, \
                     opt.nhid).expand(opt.nlayers, batch_size, 100, opt.nhid).clone().view(opt.nlayers, -1, opt.nhid))
             hidden_replicated = tuple(hidden_replicated)
