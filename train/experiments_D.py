5837981a7331fab3bf794ec29ec5e8f111d79985
Smit <sthakkar1407@gmail.com>
2019-11-20 12:01:36 -0800

    int val variable

--- train/experiments_D.py
+++ train/experiments_D.py
@@ -15,8 +15,8 @@ early_stop_val = opt.early_stop_val
 sigmas = [0.1, 0.2, 0.5, 0.8, 1.0]
 alphas = [0.05, 0.1, 0.2, 0.5]
 
-
+val = 10
 for sigma in sigmas:
     for alpha in alphas:
-        os.system("python train_D.py --cuda --path_to_home '"+ path_to_home + "' --num_val " + str(10) + " --early_stop " + str(early_stop_train) + " --pl_sigma " +str(sigma) + " --alpha_norm "+str(alpha) +" --exp_name 'rank_sig:{}_alpha:{}'".format(sigma,alpha))
+        os.system("python train_D.py --cuda --path_to_home '"+ path_to_home + "' --num_val " + str(val) + " --early_stop " + str(early_stop_train) + " --pl_sigma " +str(sigma) + " --alpha_norm "+str(alpha) +" --exp_name 'rank_sig:{}_alpha:{}'".format(sigma,alpha))
         os.system("python eval_D.py --cuda --path_to_home '"+ path_to_home + "' --early_stop " + str(early_stop_val) + " --model_path '../train/save/rank_sig:{}_alpha:{}/epoch_5.pth'".format(sigma,alpha))
