77a5635e3324670c444650831e545849719bfbe1
Smit <sthakkar1407@gmail.com>
2019-11-20 12:38:28 -0800

    small arrays for sigma and alpha

--- train/experiments_D.py
+++ train/experiments_D.py
@@ -12,8 +12,11 @@ path_to_home = opt.path_to_home
 early_stop_train = opt.early_stop_train
 early_stop_val = opt.early_stop_val
 
-sigmas = [0.1, 0.2, 0.5, 0.8, 1.0]
-alphas = [0.05, 0.1, 0.2, 0.5]
+# sigmas = [0.1, 0.2, 0.5, 0.8, 1.0]
+# alphas = [0.05, 0.1, 0.2, 0.5]
+
+sigmas = [0.1]
+alphas = [0.2]
 
 val = 10
 for sigma in sigmas:
