de74ba1806913c971f1bcaee10692c3e0eab894d
Smit <sthakkar1407@gmail.com>
2019-11-20 11:25:13 -0800

    parser path to home datas size in arguments

--- train/experiments_D.py
+++ train/experiments_D.py
@@ -1,10 +1,21 @@
 import os
+import argparse
 
-path_to_home = ''
+parser = argparse.ArgumentParser()
+parser.add_argument('--path_to_home',type=str)
+parser.add_argument('--early_stop_train', type=int,default=80000)
+parser.add_argument('--early_stop_val', type=int, default=40000)
+
+opt = parser.parse_args()
+path_to_home = opt.path_to_home
+
+early_stop_train = opt.early_stop_train
+early_stop_val = opt.early_stop_val
+
 sigmas = [0.1, 0.2, 0.5, 0.8, 1.0]
 alphas = [0.05, 0.1, 0.2, 0.5]
-early_stop_train = 500
-early_stop_val = 500
+
+
 for sigma in sigmas:
     for alpha in alphas:
         os.system("python train_D.py --cuda --path_to_home '"+ path_to_home + "' --early_stop " + str(early_stop_train) + " --pl_sigma " +str(sigma) + " --alpha_norm "+str(alpha) +" --exp_name 'rank_sig:{}_alpha:{}'".format(sigma,alpha))
